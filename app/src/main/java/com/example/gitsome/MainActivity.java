a0bde3fed9d52a638057ae7a77d393a95ff55b99
Josh Maguire <maguirejosh7@gmail.com>
2018-11-04 14:53:16 -0700

    Logcat Debug message

--- app/src/main/java/com/example/gitsome/MainActivity.java
+++ app/src/main/java/com/example/gitsome/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.gitsome;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.util.Log;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -29,6 +30,7 @@ public class MainActivity extends AppCompatActivity
             public void onClick(View view) {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
+                Log.d("MainActivity", "Add an item to the action bar");
             }
         });
 
